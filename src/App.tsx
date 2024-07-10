bc46a9d0e3ab52fba26f8278fa4294c909ea64fb
Maria Kazakova <mariakazakova92@gmail.com>
2024-07-10 16:41:43 +0300

    centered elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import {IndicatorWrapper} from "./onOffIndicator/IndicatorWrapper.tsx";
 import {MySelectorWrapper} from "./MySelector/MySelectorWrapper.tsx";
-
+import './App.css'
 
 function App() {
 
 
     return (
-        <>
+        <div className={'wrapper'}>
             <IndicatorWrapper/>
             <MySelectorWrapper/>
-        </>
+        </div>
     )
 }
 
