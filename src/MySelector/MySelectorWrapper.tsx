076a11710d1e0643a44bd93f4c798c90858f1be6
Maria Kazakova <mariakazakova92@gmail.com>
2024-07-10 17:06:40 +0300

    added usestate with SelectedItem

--- src/MySelector/MySelectorWrapper.tsx
+++ src/MySelector/MySelectorWrapper.tsx
@@ -1,39 +1,42 @@
 import style from './MySelector.module.css'
-import {MouseEventHandler, useState} from "react";
+import {useState} from "react";
 
 type CityDataType = {
     id: number
     name: string
-    isSelected: boolean
 }
 
 export const MySelectorWrapper = () => {
 
     const cities: CityDataType[] = [
-        {id: 1, name: 'Moscow', isSelected: true},
-        {id: 2, name: 'Minsk', isSelected: false},
-        {id: 3, name: 'Warsaw', isSelected: false},
-        {id: 4, name: 'Kyiv', isSelected: false},
+        {id: 1, name: 'Moscow'},
+        {id: 2, name: 'Minsk'},
+        {id: 3, name: 'Warsaw'},
+        {id: 4, name: 'Kyiv'},
     ];
 
     const [items, setItems] = useState<CityDataType[]>(cities);
     const [isOpen, setIsOpen] = useState(false);
+    const [selectedItem, setSelectedItem] = useState<CityDataType>(items[0])
 
 
     const openSelect = () => {
         setIsOpen(!isOpen)
     }
-
-    const selectItem = (id: number) => {
-        // setItems([...items, items.filter(i => i.id === id).])
+    const onClickHandler = (id: number) => {
+        const selectedItem = items.find(i => i.id === id)
+      if(selectedItem){
+          setSelectedItem(selectedItem)
+      }
     }
+
 
     return (
         <div className={style.wrapper} onClick={openSelect}>
             {isOpen ? (
-                    <div>{items.map((i, index) => <div key={index} className={style.listItem} onClick={selectItem(i.id)}>{i.name}</div>)}</div>)
+                    <div className={style.list}>{items.map((i, index) => <div key={index} className={style.listItem} onClick={() => onClickHandler(i.id)}>{i.name}</div>)}</div>)
                 :
-                <div className={style.listItem}>{items[0].name}</div>}
+                <div className={style.listItem}>{selectedItem.name}</div>}
         </div>
     )
 }
